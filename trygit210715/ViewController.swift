e5f88c0cde20f5d6f26a43fb6ec825cc937d0bcc
smplcod <smplcod2021@gmail.com>
2021-07-15 18:14:08 +0400

    9

--- trygit210715/ViewController.swift
+++ trygit210715/ViewController.swift
@@ -3,22 +3,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Code 1
-        /*
-         code
-         */
-        
-        // Code 2 New
-        /*
-         code2new
-         */
-        
-        // App Store 1
-        
-        // Code 4
-        
-        // Code 5
-        
-        // code 6
+        // 9
     }
 }
