1213edcdf83091743f4e0532c83b1cc20ac07327
smplcod <smplcod2021@gmail.com>
2021-07-15 16:44:26 +0400

    code 1

--- trygit210715/ViewController.swift
+++ trygit210715/ViewController.swift
@@ -2,6 +2,11 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Code 1
+        /*
+         code
+         */
     }
 }
 
